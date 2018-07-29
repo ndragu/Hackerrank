1b40ee23aa4de77c63e06781f20a6273ee1b61be
Nicolae Dragu <nick.dragu@gmail.com>
2018-07-29 11:26:15 +0300

    Update 01-SimpleArraySum.js

--- Algorithms/Wamup/01-SimpleArraySum.js
+++ Algorithms/Wamup/01-SimpleArraySum.js
@@ -1,3 +1,6 @@
+//Given an array of integers, find the sum of its elements.
+//For example, if the array ar=[1,2,3], 1+2+3=6, so it returns 6.
+
 simpleArraySum([1,2,3,4,10,11]);
 
 function simpleArraySum(ar) {
