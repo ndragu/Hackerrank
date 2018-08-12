669a4ba811c251507ad64b03f73c5ab72092b9e5
Nicolae Dragu <nick.dragu@gmail.com>
2018-08-12 18:03:33 +0300

    Update GradingStudents.js

--- Algorithms/Implementation/GradingStudents.js
+++ Algorithms/Implementation/GradingStudents.js
@@ -1,14 +1,17 @@
+/*
+Given an array of grades, return an array of rounded grades based on the following two rules:
+1. If the difference between the grade and the next multiple of 5 is less than 3, round up to the next multiple of 5.
+2. If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+*/
 function gradingStudents(grades) {
     var newGrades=[];
     for (var i=0; i<grades.length; i++) {
         var multOfFive=Math.ceil(grades[i]/5);
         var roundingCutoff=multOfFive*5-grades[i];
-        if (grades[i]<38) {
-            //console.log(grades[i]);
+        if (grades[i]<38) {           
             newGrades.push(grades[i]);
         } else {
-            if (roundingCutoff<3) {
-                //console.log(multOfFive*5);
+            if (roundingCutoff<3) {                
                 newGrades.push(multOfFive*5);
             } else {
                 newGrades.push(grades[i]);
